0e20bfb4e7b9c525c345d9c0e0c3b75ba44745f9
Frost Developer <cg.help.developers@gmail.com>
2019-03-07 18:45:18 +0530

    updated code

---
+++ app/src/main/java/com/maya/testfrost/constants/VideoPlayerStage.java
@@ -0,0 +1,12 @@
+package com.maya.testfrost.constants;
+
+public enum VideoPlayerStage
+{
+    PIP,  // ABOVE OREO
+    STABLE,
+    MIN_SCREEN,
+    FULL_SCREEN,
+    FLOATING,
+
+
+}
--- app/src/main/java/com/maya/testfrost/fragments/VideoPlayerFragment.java
+++ app/src/main/java/com/maya/testfrost/fragments/VideoPlayerFragment.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
@@ -30,6 +31,9 @@ import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.LoopingMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
 import com.google.android.exoplayer2.source.hls.DefaultHlsExtractorFactory;
 import com.google.android.exoplayer2.source.hls.HlsMediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
@@ -43,6 +47,7 @@ import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultAllocator;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 import com.maya.testfrost.R;
 import com.maya.testfrost.constants.Constants;
@@ -98,9 +103,6 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
     private BandwidthMeter bandwidthMeter;
 
 
-
-
-
     public VideoPlayerFragment() {
         // Required empty public constructor
     }
@@ -128,14 +130,13 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_video_player, container, false);
-        ButterKnife.bind(this,view);
+        ButterKnife.bind(this, view);
 
         setUp();
         return view;
     }
 
-    private void setUp()
-    {
+    private void setUp() {
         shouldAutoPlay = true;
         bandwidthMeter = new DefaultBandwidthMeter();
         window = new Timeline.Window();
@@ -145,17 +146,16 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
             activity().onBackPressed();
         });
 
-        imgResize.setOnClickListener(v ->{
-            if(playerView!=null)
-            {
-                imgResize.setImageResource(activity().getRequestedOrientation()==ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE?R.drawable.exo_controls_fullscreen_enter:R.drawable.exo_controls_fullscreen_exit);
-                activity().setRequestedOrientation(activity().getRequestedOrientation()==ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                //hideSystemUi();
+        imgResize.setOnClickListener(v ->
+        {
+            if (playerView != null) {
+                imgResize.setImageResource(activity().getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE ? R.drawable.exo_controls_fullscreen_enter : R.drawable.exo_controls_fullscreen_exit);
+                activity().setRequestedOrientation(activity().getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+                //decorateUi();
             }
 
         });
-
-
 
 
     }
@@ -163,12 +163,10 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
 
     private void initializePlayer() {
 
-        if(uri==null)
-        {
+        if (uri == null) {
             return;
         }
-        if(player!=null)
-        {
+        if (player != null) {
             return;
         }
         playerView.requestFocus();
@@ -176,31 +174,24 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
         bandwidthMeter = new DefaultBandwidthMeter();
 
 
-
         TrackSelection.Factory videoTrackSelectionFactory =
                 new AdaptiveTrackSelection.Factory();
 
         trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
-        DefaultAllocator allocator = new DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE);
         // Here
 
         player = ExoPlayerFactory.newSimpleInstance(activity(), trackSelector);
+        playerView.setControllerHideOnTouch(true);
+        playerView.setControllerShowTimeoutMs(1500);
         playerView.setPlayer(player);
         player.setPlayWhenReady(shouldAutoPlay);
 
-        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(activity(), Util.getUserAgent(activity(), "IPOLL"));
 
-//        MediaSource mediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(uri);
-//        player.prepare(mediaSource);
 
-        DefaultHlsExtractorFactory defaultHlsExtractorFactory = new DefaultHlsExtractorFactory(DefaultTsPayloadReaderFactory.FLAG_ALLOW_NON_IDR_KEYFRAMES);
-        MediaSource mediaSource = new HlsMediaSource.Factory(dataSourceFactory)
-                .setExtractorFactory(defaultHlsExtractorFactory)
-                .createMediaSource(uri);
+        MediaSource mediaSource = buildMediaSource(uri);
 
         LoopingMediaSource loopingSource = new LoopingMediaSource(mediaSource);
         player.prepare(loopingSource);
-
 
 
         player.addListener(new Player.EventListener() {
@@ -221,8 +212,7 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
 
             @Override
             public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
-                switch (playbackState)
-                {
+                switch (playbackState) {
                     case Player.STATE_IDLE:
                         progressBar.setVisibility(View.GONE);
                         break;
@@ -273,6 +263,27 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
         });
     }
 
+
+    private MediaSource buildMediaSource(Uri uri)
+    {
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(activity(), Util.getUserAgent(activity(), "IPOLL"));
+
+        if (uri.getLastPathSegment().contains("mp3") || uri.getLastPathSegment().contains("mp4")) {
+            return new ExtractorMediaSource.Factory(new DefaultHttpDataSourceFactory("IPOLL"))
+                    .createMediaSource(uri);
+        } else if (uri.getLastPathSegment().contains("m3u8")) {
+            DefaultHlsExtractorFactory defaultHlsExtractorFactory = new DefaultHlsExtractorFactory(DefaultTsPayloadReaderFactory.FLAG_ALLOW_NON_IDR_KEYFRAMES);
+            return new HlsMediaSource.Factory(dataSourceFactory)
+                    .setExtractorFactory(defaultHlsExtractorFactory)
+                    .createMediaSource(uri);
+        } else {
+            DashChunkSource.Factory dashChunkSourceFactory = new DefaultDashChunkSource.Factory(dataSourceFactory);
+            DataSource.Factory manifestDataSourceFactory = new DefaultHttpDataSourceFactory("IPOLL");
+            return new DashMediaSource.Factory(dashChunkSourceFactory, manifestDataSourceFactory).createMediaSource(uri);
+        }
+    }
+
     private void releasePlayer() {
         if (player != null) {
             shouldAutoPlay = player.getPlayWhenReady();
@@ -283,12 +294,10 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
     }
 
 
-
     @Override
     public void onStart() {
         super.onStart();
-        if (player == null)
-        {
+        if (player == null) {
             initializePlayer();
         }
     }
@@ -296,12 +305,9 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
     @Override
     public void onResume() {
         super.onResume();
-        if (player == null)
-        {
+        if (player == null) {
             initializePlayer();
-        }
-        else
-        {
+        } else {
             shouldAutoPlay = true;
             player.setPlayWhenReady(true);
         }
@@ -310,12 +316,9 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
     @Override
     public void onPause() {
         super.onPause();
-        if (player == null)
-        {
+        if (player == null) {
 
-        }
-        else
-        {
+        } else {
             shouldAutoPlay = false;
             player.setPlayWhenReady(false);
         }
@@ -324,8 +327,7 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
     @Override
     public void onStop() {
         super.onStop();
-        if (Util.SDK_INT <= 23)
-        {
+        if (Util.SDK_INT <= 23) {
             releasePlayer();
         }
 
@@ -343,7 +345,21 @@ public class VideoPlayerFragment extends Fragment implements IFragment {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION );
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+    }
+
+
+    private void decorateUi() {
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                View decorView = activity().getWindow().getDecorView();
+                int uiOptions =
+                         View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                decorView.setSystemUiVisibility(uiOptions);
+            }
     }
 
     @Override
